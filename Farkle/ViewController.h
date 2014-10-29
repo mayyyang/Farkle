aa8baa7894cfe4fdbcbd32efb24e0f27d9be3414
May Yang <mayyang@May-Yangs-MacBook-Pro.local>
2014-10-29 13:43:20 -0700

    Initial Commit

---
+++ Farkle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by May Yang on 10/29/14.
+//  Copyright (c) 2014 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
